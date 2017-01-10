e17464b35eb46d7bf73810d420b6f5e6925106db
Кузьмин Алексей александрович <akuzmin@jet.msk.su>
2017-01-10 09:53:33 +0300

    test

--- src/main/java/com/acme/edu/Main.java
+++ src/main/java/com/acme/edu/Main.java
@@ -27,6 +27,7 @@ public class Main {
         float f = 0.0F;
         double d = 1e4;
         System.out.println(.1 +.2); //IEEE 
+        System.out.println(.1 +.3); //IEEE 
         
         boolean bb = true | false;    
         
