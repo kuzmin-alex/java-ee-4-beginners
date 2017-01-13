c31bf122c67a271c50e71ea6644c2d100793d90c
Кузьмин Алексей александрович <akuzmin@jet.msk.su>
2017-01-13 17:37:45 +0300

    qweqweqwqw

--- src/main/java/su/jet/java/edu/loader/Run.java
+++ src/main/java/su/jet/java/edu/loader/Run.java
@@ -16,6 +16,7 @@ public class Run {
         DataLoader dynamicDataLoader = new DataLoader(
                 new DataWriter[]{new DbDataWriter(), new LdapDataWriter()},
                 new CsvDataReader()
+                dfads
         );
 
         dynamicDataLoader.doRead();
